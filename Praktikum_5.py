a6250cad02af4e1cd259627ccd876e2e5e3ccac0
Miftahu Rizkiyah <mtrizkiyah@gmail.com>
2020-11-25 22:00:15 +0700

    praktikum 5

---
+++ Praktikum_5.py
@@ -0,0 +1,76 @@
+import os,sys
+Oc = os.system
+while True:
+    print("")
+    print("")
+    c = input("(L)ihat, (T)ambah, (U)bah, (H)apus, (C)ari, (K)eluar: ")
+    if c.lower() == 'k':
+        break
+    elif c.lower() == 'l':
+        i = open('database.txt','r').read().splitlines()
+        print(" ╔═════════════════════════════════════════════════════════════════════╗")
+        print(" ╠════════════════════════════ DAFTAR NILAI  ══════════════════════════╣")
+        print(" ╠══════════════════╦══════════════════╦═══════╦═══════╦═══════╦═══════╣")
+        print(" ║      NAMA        ║       NIM        ║ TUGAS ║  UTS  ║  UAS  ║ AKHIR ║")
+        print(" ╠══════════════════╬══════════════════╬═══════╬═══════╬═══════╬═══════╣")
+        for l in i:
+            if l == '':
+                pass
+            elif i=="":
+                print("Tidak ada data")
+            else:
+                l1 = l.replace('Nama : ','').replace('Nim : ','').replace('Tugas : ','').replace('UTS : ','').replace('UAS : ','').replace('Akhir : ','')
+                na,ni,tu,uts,uas,akhir = l1.strip().split('|')
+                print((' ║ ')+(na[:15]).ljust(17,'.')+('║ ')+(ni).ljust(17)+('║ ')+(tu).ljust(6)+('║ ')+(uts).ljust(6)+('║ ')+(uas).ljust(6)+('║ ')+(akhir).ljust(6)+('║'))
+        print(" ╚══════════════════╩══════════════════╩═══════╩═══════╩═══════╩═══════╝")
+    elif c.lower() == 'c':
+        cari = input(' Mencari : ')
+        i = open('database.txt','r').read().splitlines()
+        print(" ╔═════════════════════════════════════════════════════════════════════╗")
+        print(" ╠════════════════════════════ DAFTAR NILAI  ══════════════════════════╣")
+        print(" ╠══════════════════╦══════════════════╦═══════╦═══════╦═══════╦═══════╣")
+        print(" ║      NAMA        ║       NIM        ║ TUGAS ║  UTS  ║  UAS  ║ AKHIR ║")
+        print(" ╠══════════════════╬══════════════════╬═══════╬═══════╬═══════╬═══════╣")
+        for l in i:
+            if l == '':
+                pass
+            elif cari in l:
+                l1 = l.replace('Nama : ','').replace('Nim : ','').replace('Tugas : ','').replace('UTS : ','').replace('UAS : ','').replace('Akhir : ','')
+                na,ni,tu,uts,uas,akhir = l1.strip().split('|')
+                print((' ║ ')+(na).ljust(17)+('║ ')+(ni).ljust(17)+('║ ')+(tu).ljust(6)+('║ ')+(uts).ljust(6)+('║ ')+(uas).ljust(6)+('║ ')+(akhir).ljust(6)+('║'))
+        print(" ╚══════════════════╩══════════════════╩═══════╩═══════╩═══════╩═══════╝")
+    elif c.lower() == 'h':
+        u = open('database.txt','r').read().splitlines()
+        target = input(' Masukan Nama : ')
+        nm = []
+        for l in u:
+            if l == '':
+                pass
+            else:
+                l1 = l.replace('Nama : ','').replace('Nim : ','').replace('Tugas : ','').replace('UTS : ','').replace('UAS : ','').replace('Akhir : ','')
+                na,ni,tu,uts,uas,akhir = l1.strip().split('|')
+                if str(na) == str(target):
+                    print('BERHASIL MENGHAPUS Data %s'%(target))
+                    pass
+                else:
+                    nm.append(str(l)+'\n')
+        new = open('database.txt','w')
+        new.write(str(nm))
+        new.close()
+        new = open('database.txt','r').read().splitlines()
+        new1 = open('database.txt','w')
+        new1.close()
+        new2 = open('database.txt','a')
+        for i in new:
+            i2 = i.replace("['","").replace("\\n', '", "\n").replace("']","").replace("\\n",'')
+            new2.write(i2)
+        new2.close()
+    elif c.lower() == 'u':
+        u = open('database.txt','r').read().splitlines()
+        target = input(' Masukan Nama : ')
+        nm = []
+        for l in u:
+            if l == '':
+                pass
+            else:
+                l1 = l.replace('Nama : ','').replace('Nim : ','').replace('Tugas : ','')
